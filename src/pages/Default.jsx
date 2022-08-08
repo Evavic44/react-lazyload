90be28a9874be337f13c9708802cb424f65741fe
Victor Eke <evavic44@gmail.com>
2022-08-08 19:11:18 +0100

    :zap: add 100vh to header

--- src/pages/Default.jsx
+++ src/pages/Default.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Image from "../images/bird.jpg";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export default function Default() {
   return (
     <div className="container">
       <header>
         <h1>Default</h1>
-        <p>No lazyload, just a plain image using the img tag</p>
+        <p>Lazyload by default, without any effects</p>
         <Link to="/">Go back</Link>
       </header>
 
       <div className="card">
-        <img
+        <LazyLoadImage
           className="image"
           src={Image}
           width={800}
           height={550}
-          alt="Fishy"
+          alt="bird"
         />
       </div>
     </div>
